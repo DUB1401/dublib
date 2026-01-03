9a2fdbebc7e02b682a7458537c8d0d8229378448
DUB1401 <vlad.milosta@yandex.ru>
2026-01-04 00:03:55 +0300

    Add: color logs printing

--- src/dublib/Core.py
+++ src/dublib/Core.py
@@ -1,6 +1,27 @@
-from .CLI.TextStyler import GetStyledTextFromHTML
+from .CLI.TextStyler import FastStyler, GetStyledTextFromHTML
 
 import logging
 
+class ColorFormatter(logging.Formatter):
+	"""Форматировщик вывода в консоль с поддержкой цветов."""
+
+	def format(self, record: logging.LogRecord) -> str:
+		"""
+		Форматирует записи с поддержкой цветной палитры.
+
+		:param record: Запись лога.
+		:type record: LogRecord
+		:return: Текст сообщения.
+		:rtype: str
+		"""
+		
+		Message = super().format(record)
+
+		match record.levelno: 
+			case logging.WARNING: Message = FastStyler(Message).colorize.yellow
+			case logging.ERROR: Message = FastStyler(Message).colorize.red
+
+		return Message
+
 LOGS_HANDLER = logging.StreamHandler()
-LOGS_HANDLER.setFormatter(logging.Formatter(GetStyledTextFromHTML("<b>[%(name)s]</b> %(levelname)s: %(message)s")))
+LOGS_HANDLER.setFormatter(ColorFormatter(GetStyledTextFromHTML("<b>[%(name)s]</b> %(levelname)s: %(message)s")))
