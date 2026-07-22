c025a6237fce314ebf8bfa45f70f0dd8b6572846
DUB1401 <vlad.milosta@yandex.ru>
2026-07-22 11:32:59 +0300

    Add: clear history option

--- src/dublib/Methods/System.py
+++ src/dublib/Methods/System.py
@@ -1,5 +1,5 @@
+import subprocess
 import sys
-import os
 
 #==========================================================================================#
 # >>>>> ФУНКЦИИ РАБОТЫ С СИСТЕМОЙ <<<<< #
@@ -8,26 +8,42 @@ import os
 def CheckPythonMinimalVersion(major: int, minor: int, raise_exception: bool = True) -> bool:
 	"""
 	Проверяет, соответствует ли используемая версия Python минимальной требуемой.
-		major – идентификатор Major-версии Python;\n
-		minor – идентификатор Minor-версии Python;\n
-		raise_exception – указывает, как поступать при несоответствии версии: выбрасывать исключение или возвращать значение.
+
+	:param major: Идентификатор Major-версии Python.
+	:type major: int
+	:param minor: Идентификатор Minor-версии Python.
+	:type minor: int
+	:param raise_exception: Указывает, как поступать при несоответствии версии: выбрасывать исключение или возвращать значение.
+	:type raise_exception: bool
+	:raises RuntimeError: Неподдерживаемая версия Python.
 	"""
 
-	IsVersionCorrect = True
-	
 	if sys.version_info < (major, minor): 
-		if raise_exception: raise RuntimeError(f"Python {major}.{minor} or newer is required.")
-		else: IsVersionCorrect = False
+		if raise_exception:
+			raise RuntimeError(f"Python {major}.{minor} or newer is required.")
+		else:
+			return False
 
-	return IsVersionCorrect
+	return True
 
-def Clear():
-	"""Очищает консоль."""
+def Clear(clear_history: bool = True):
+	"""
+	Очищает терминал.
 
-	os.system("cls" if os.name == "nt" else "clear")
+	:param clear_history: Указывает, нужно ли удалить также и буфер обратной прокрутки терминала.
+	:type clear_history: bool
+	"""
+
+	Operation: str = "\033[H\033[2J"
+	if clear_history: Operation += "\033[3J"
+	sys.stdout.write(Operation)
+	sys.stdout.flush()
 
 def Shutdown():
 	"""Выключает устройство."""
-
-	if sys.platform in ["linux", "linux2"]: os.system("sudo shutdown now")
-	if sys.platform == "win32": os.system("shutdown /s")
+	
+	if sys.platform == "linux":
+		subprocess.run(("sudo", "shutdown", "now"))
+		
+	elif sys.platform == "win32":
+		subprocess.run(("shutdown", "/s", "/t", "0"))
